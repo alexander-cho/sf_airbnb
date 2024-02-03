640f74b581de30550e2b507d7f7f561002309607
Alex Cho <alex.jhcho@gmail.com>
2024-02-02 17:36:55 -0800

    random forest

--- models.R
+++ models.R
@@ -1,6 +1,8 @@
 library(tidymodels)
 library(tidyverse)
 library(kknn)
+library(ranger)
+library(vip)
 
 # read in clean data
 sf_airbnb_clean <- read_csv("sf_airbnb_clean.csv")
@@ -119,5 +121,61 @@ augment(knn_final_fit, new_data = sf_airbnb_test) %>%
 saveRDS(knn_final_fit, file = "models/knn_model.rds")
 saveRDS(knn_tune_res, file = "models/knn_tune_res.rds")
 
+# Random Forest
 
+# specify random forest model
+rf_model <- rand_forest(mtry = tune(),
+                        trees = 1000,
+                        min_n = tune()) %>%
+  set_mode("regression") %>%
+  set_engine("ranger", importance = "impurity")
+
+# specify workflow
+rf_workflow <- workflow() %>%
+  add_model(rf_model) %>%
+  add_recipe(sf_airbnb_recipe)
+
+# specify hyperparameters
+rf_tune_res <- tune_grid(rf_workflow,
+                         resamples = sf_airbnb_folds,
+                         grid = expand_grid(
+                           mtry = seq(2, 10, length.out = 5),
+                           min_n = seq(1, 10, length.out = 5)
+                         ))
+
+# plot results
+autoplot(rf_tune_res, metric = "rmse")
+
+# select the best model and fit
+rf_final_fit <- rf_tune_res %>%
+  select_best("rmse") %>%
+  finalize_workflow(rf_workflow, .) %>%
+  fit(sf_airbnb_train)
+
+# plot actual vs. predicted scores
+augment(rf_final_fit, new_data = sf_airbnb_test) %>% 
+  ggplot(aes(x = .pred, y = price)) +
+  geom_point(alpha = 0.4) +
+  geom_abline(lty = 2, color = "red") +
+  coord_cartesian(xlim = c(0, 1000),
+                  ylim = c(0, 1000)) +
+  labs(title = "Random forest: actual vs. predicted prices",
+       x = "Predicted price",
+       y = "Actual price")
+
+# calculate RMSE
+augment(rf_final_fit, new_data = sf_airbnb_test) %>%
+  rmse(truth = price, estimate = .pred)
+
+# variable importance plot
+rf_vip <- rf_final_fit %>%
+  extract_fit_parsnip() %>%
+  vip(num_features = 15) + 
+  labs(title = "Random forest: variable importance",
+       x = "Predictors",
+       y = "Importance")
+
+# save final model
+save(rf_tune_res, file = "models/rf_tune_res.rda")
+save(rf_final_fit, file = "models/rf_fit.rda")
 
